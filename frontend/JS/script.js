5d817668c420225fd3e93bd8b9bde0119b4387b0
quivei13 <joacoxnaveillan@gmail.com>
2023-09-17 11:39:30 -0300

    creacion de menu principal // correccion de error formulario

--- frontend/JS/script.js
+++ frontend/JS/script.js
@@ -1,20 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
     const form = document.getElementById("login-form");
+    
+    // NO TOCAR
+    // codigo del formulario de inicio de sesion 
+    if (form) { // Verifica si el formulario existe en esta página
+        form.addEventListener("submit", function (e) {
+            e.preventDefault();
 
-    form.addEventListener("submit", function (e) {
-        e.preventDefault();
+            // Aquí puedes agregar la lógica de autenticación
+            const username = document.getElementById("username").value;
+            const password = document.getElementById("password").value;
 
-        // Aquí puedes agregar la lógica de autenticación
-        const username = document.getElementById("username").value;
-        const password = document.getElementById("password").value;
-
-        // Ejemplo de autenticación básica
-        if (username === "usuario" && password === "contraseña") {
-            alert("Inicio de sesión exitoso");
-            // Redireccionar a la página principal
-            window.location.href = "menu.html";
-        } else {
-            alert("Credenciales incorrectas. Inténtalo de nuevo.");
-        }
-    });
+            // Ejemplo de autenticación básica
+            if (username === "usuario" && password === "contraseña") {
+                alert("Inicio de sesión exitoso");
+                // Redireccionar a la página principal
+                window.location.href = "menu_principal.html";
+            } else if (username === "administrador" && password === "contraseña") {
+                alert("Inicio de sesión exitoso");
+                // Redireccionar a la página principal
+                window.location.href = "menu.html";
+            } else {
+                alert("Credenciales incorrectas. Inténtalo de nuevo.");
+            }
+        });
+    }
 });
