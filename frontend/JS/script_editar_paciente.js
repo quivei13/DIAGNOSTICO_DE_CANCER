9ebf6cf6e1e7c382cd1becbdb50e35551e06dc5d
quivei13 <joacoxnaveillan@gmail.com>
2023-09-21 20:22:36 -0300

    apariencia de ventanas completa (falta ordenar el codigo)

---
+++ frontend/JS/script_editar_paciente.js
@@ -0,0 +1,8 @@
+// Selecciona el botón "Volver" por su ID
+const botonVolver = document.getElementById("boton-volver");
+
+// Agrega un evento de clic al botón "Volver"
+botonVolver.addEventListener("click", function () {
+    // Utiliza la función window.history.back() para regresar a la página anterior
+    window.history.back();
+});
