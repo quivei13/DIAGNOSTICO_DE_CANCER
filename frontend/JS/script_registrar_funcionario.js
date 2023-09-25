d6cfdc6259f5922aeececc76a09f3377d975651b
quivei13 <joacoxnaveillan@gmail.com>
2023-09-25 19:48:55 -0300

    pagina prediccion - boton limpiar campos - ordenar codigo

--- frontend/JS/script_registrar_funcionario.js
+++ frontend/JS/script_registrar_funcionario.js
@@ -1,3 +1,5 @@
+//BOTON VOLVER
+////////////////////////////////////////////////////////////////////////////////////////
 // Selecciona el botón "Volver" por su ID
 const botonVolver = document.getElementById("boton-volver");
 
@@ -6,3 +8,28 @@ botonVolver.addEventListener("click", function () {
     // Utiliza la función window.history.back() para regresar a la página anterior
     window.history.back();
 });
+////////////////////////////////////////////////////////////////////////////////////////
+
+
+//BOTON LIMPIAR CAMPOS
+////////////////////////////////////////////////////////////////////////////////////////
+// Selecciona el botón "Limpiar Campos" por su ID
+const botonLimpiarCampos = document.getElementById("limpiar-campos");
+
+// Selecciona el formulario por su ID
+const formulario = document.getElementById("registrar-funcionario-form"); // Reemplaza "tu-formulario-id" con el ID de tu formulario
+
+// Agrega un evento de clic al botón "Limpiar Campos"
+botonLimpiarCampos.addEventListener("click", function () {
+    // Recorre todos los elementos del formulario
+    const elementosFormulario = formulario.elements;
+    for (let i = 0; i < elementosFormulario.length; i++) {
+        const elemento = elementosFormulario[i];
+        // Verifica si el elemento es un campo de entrada (input) o un área de texto (textarea)
+        if (elemento.tagName === "INPUT" || elemento.tagName === "TEXTAREA") {
+            // Limpia el valor del campo
+            elemento.value = "";
+        }
+    }
+});
+////////////////////////////////////////////////////////////////////////////////////////
--- frontend/JS/script_registrar_paciente.js
+++ frontend/JS/script_registrar_paciente.js
@@ -1,3 +1,5 @@
+//BOTON VOLVER
+////////////////////////////////////////////////////////////////////////////////////////
 // Selecciona el botón "Volver" por su ID
 const botonVolver = document.getElementById("boton-volver");
 
@@ -6,9 +8,12 @@ botonVolver.addEventListener("click", function () {
     // Utiliza la función window.history.back() para regresar a la página anterior
     window.history.back();
 });
+////////////////////////////////////////////////////////////////////////////////////////
 
 
 
+//BOTON REGISTRAR PACIENTE
+////////////////////////////////////////////////////////////////////////////////////////
 // Selecciona el botón "Registrar Paciente" por su ID
 const botonRegistrar = document.getElementById("boton-registrar");
 
@@ -26,4 +31,29 @@ botonRegistrar.addEventListener("click", function () {
 function mostrarMensajeConfirmacion() {
     
 }
+////////////////////////////////////////////////////////////////////////////////////////
 
+
+
+//BOTON LIMPIAR CAMPOS
+////////////////////////////////////////////////////////////////////////////////////////
+// Selecciona el botón "Limpiar Campos" por su ID
+const botonLimpiarCampos = document.getElementById("limpiar-campos");
+
+// Selecciona el formulario por su ID
+const formulario = document.getElementById("registrar-paciente-form"); // Reemplaza "tu-formulario-id" con el ID de tu formulario
+
+// Agrega un evento de clic al botón "Limpiar Campos"
+botonLimpiarCampos.addEventListener("click", function () {
+    // Recorre todos los elementos del formulario
+    const elementosFormulario = formulario.elements;
+    for (let i = 0; i < elementosFormulario.length; i++) {
+        const elemento = elementosFormulario[i];
+        // Verifica si el elemento es un campo de entrada (input) o un área de texto (textarea)
+        if (elemento.tagName === "INPUT" || elemento.tagName === "TEXTAREA") {
+            // Limpia el valor del campo
+            elemento.value = "";
+        }
+    }
+});
+////////////////////////////////////////////////////////////////////////////////////////
