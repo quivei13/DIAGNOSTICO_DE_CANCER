da0b7a48e19ce3c085dc8ab6b8b65bfd0c31593b
quivei13 <joacoxnaveillan@gmail.com>
2023-09-27 18:43:12 -0300

    correcciones de apariencia excepto del ultimo sprint

--- frontend/JS/script_buscar_paciente.js
+++ frontend/JS/script_buscar_paciente.js
@@ -9,3 +9,26 @@ botonVolver.addEventListener("click", function () {
     window.history.back();
 });
 ////////////////////////////////////////////////////////////////////////////////////////
+
+
+//CHECKBOX FILTROS
+////////////////////////////////////////////////////////////////////////////////////////
+// Selecciona todos los checkboxes con la clase "habilitar-input"
+const checkboxes = document.querySelectorAll(".habilitar-input");
+
+// Agrega un evento de cambio a cada checkbox
+checkboxes.forEach(function (checkbox) {
+    checkbox.addEventListener("change", function () {
+        // Obtiene el ID del campo de entrada objetivo desde el atributo "data-target"
+        const targetId = checkbox.getAttribute("data-target");
+        const campoDeEntrada = document.getElementById(targetId);
+
+        // Habilita o deshabilita el campo de entrada según el estado del checkbox
+        campoDeEntrada.disabled = !checkbox.checked;
+    });
+});
+////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
