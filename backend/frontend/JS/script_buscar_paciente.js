7bb376f8349665897201446d822b1491bcb64f1d
quivei13 <joacoxnaveillan@gmail.com>
2023-10-02 18:12:35 -0300

    buscar_paciente.html carga los pacientes de la base de datos

--- backend/frontend/JS/script_buscar_paciente.js
+++ backend/frontend/JS/script_buscar_paciente.js
@@ -30,5 +30,52 @@ checkboxes.forEach(function (checkbox) {
 ////////////////////////////////////////////////////////////////////////////////////////
 
 
+//CARGA DE TABLA PACIENTES
+////////////////////////////////////////////////////////////////////////////////////////
+function cargarPacientes() {
+    // Realizar una solicitud GET al servidor para obtener los datos de los pacientes
+    fetch('/api/pacientes', { method: 'GET' })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        // Llenar la tabla con los datos obtenidos
+        var tablaPacientes = document.getElementById('tabla-pacientes');
+        tablaPacientes.innerHTML = ''; // Limpia cualquier contenido previo
 
+        data.forEach(function (paciente) {
+          // Crea una nueva fila en la tabla para cada paciente
+          var fila = document.createElement('tr');
+          // Agrega las celdas de datos
+          fila.innerHTML = `
+            <td>${paciente.rut}</td>
+            <td>${paciente.nombre}</td>
+            <td>${paciente.apellido_paterno}</td>
+            <td>${paciente.apellido_materno}</td>
+            <td>${paciente.genero}</td>
+            <td>${paciente.fecha_de_nacimiento}</td>
+            <td>${paciente.correo_electronico}</td>
+            <td>${paciente.telefono}</td>
+            <td>${paciente.edad}</td>
+            <td>${paciente.cancer}</td>
+            <td>${paciente.diagnostico_inicial}</td>
+            <td>${paciente.radiografias}</td>
+            <td>${paciente.condiciones_fisicas}</td>
+            <td>${paciente.condiciones_ambientales}</td>
+            <td>${paciente.datos_gen_mol}</td>
+            <td>${paciente.historia_medica}</td>
+          `;
+          // Agrega la fila a la tabla
+          tablaPacientes.appendChild(fila);
+        });
+      })
+      .catch(function (error) {
+        console.error('Error al cargar pacientes:', error);
+      });
+  };
 
+  document.addEventListener("DOMContentLoaded", function () {
+    cargarPacientes();
+  });
+  
+////////////////////////////////////////////////////////////////////////////////////////
